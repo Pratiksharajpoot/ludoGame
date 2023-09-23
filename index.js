e233301fd11a2d69abf88cd33fccb6c15fc1a511
Pratiksharajpoot <116014980+Pratiksharajpoot@users.noreply.github.com>
2023-09-24 00:32:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1=Math.floor(Math.random()*6)+1;
 
 var randomDiceImage = "imgd" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "" + randomDiceImage; //dice1.png - dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -11,7 +11,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/imgd" + randomNumber2 + ".png";
+var randomImageSource2 = "imgd" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
